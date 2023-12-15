067024d605e6c3a81c35eb3786f15fe775e9b026
aceDavon <d.onate@simpsgroup.co.uk>
2023-12-15 10:44:59 +0100

    change port for deployment

--- server.py
+++ server.py
@@ -33,6 +33,6 @@ def get_weather():
 if __name__ == '__main__':
     configure_logging()
     try:
-        serve(app, host='localhost', port=8080)
+        serve(app, host='localhost', port=8000)
     except Exception as e:
         print(f"An error occurred: {e}")
